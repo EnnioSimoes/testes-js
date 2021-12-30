2e9bad346bc4a5ef61a4e5e2af05ebcef1c3a3e5
Ennio Simões <ennio.simoes@sodresantoro.com.br>
2021-12-30 18:54:26 -0300

    Lesson 10 OK

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 * 1. Criação do projeto e instalação do Jest ✓
 * 2. Introdução ao Jest e criação do primeiro teste ✓
-* 3. Jest watch mode: observar alterações nos arquivos
+* 3. Jest watch mode: observar alterações nos arquivos ✓
+* 4. Implementar library para gerar query strings com TDD ✓
+* 5. Instalar a configurar o Prettier ✓
+* 6. Refatorando com confiança ✓
+* 7. Mais casos de teste para query string ✓
+* 8. Mais refactoring com suporte dos testes ✓
+* 9. Implementar com TDD o método para fazer parse da Query String - parte 1 ✓
+* 10. Implementar com TDD o método para fazer parse da Query String - parte 2 ✓
+* 11. Test Coverage: descobrir o que falta testar
+
+
+
---
+++ projeto1/src/lib/queryString.spec.js
@@ -0,0 +1,58 @@
+const { queryString, parse } = require('./queryString');
+
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Ennio',
+      profession: 'developer',
+    };
+    expect(queryString(obj)).toBe('name=Ennio&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passsed as value', () => {
+    const obj = {
+      name: 'Ennio',
+      abilities: ['js', 'TDD'],
+    };
+    expect(queryString(obj)).toBe('name=Ennio&abilities=js,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Ennio',
+      abilities: {
+        first: 'js',
+        second: 'TDD',
+      },
+    };
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
+
+describe('Query String to Object', () => {
+  it('should convert a query to object', () => {
+    const qs = 'name=Ennio&profession=developer';
+    expect(parse(qs)).toEqual({
+      name: 'Ennio',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string of a single key-value', () => {
+    const qs = 'name=Ennio';
+    expect(parse(qs)).toEqual({
+      name: 'Ennio',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated value', () => {
+    const qs = 'name=Ennio&abilities=js,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Ennio',
+      abilities: ['js', 'TDD'],
+    });
+  });
+});
